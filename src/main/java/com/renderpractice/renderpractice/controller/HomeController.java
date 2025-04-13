28e24b102bd34908941a1e85531d089df10425fc
henrykimy1994 <henrykimy.1994@gmail.com>
2025-04-13 23:54:03 +0900

    home endpoint changed

--- src/main/java/com/renderpractice/renderpractice/controller/HomeController.java
+++ src/main/java/com/renderpractice/renderpractice/controller/HomeController.java
@@ -4,9 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
 public class HomeController {
-    @RequestMapping("/home")
+    @RequestMapping("/")
     public String home() {
         return "Hello";
     }
