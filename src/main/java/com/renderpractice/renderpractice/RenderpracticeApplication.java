d8b456f58f5281213fd2dd2efb8b225ddb945fef
henrykimy1994 <henrykimy.1994@gmail.com>
2025-04-13 23:23:24 +0900

    first commit
    first commit

---
+++ src/main/java/com/renderpractice/renderpractice/RenderpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.renderpractice.renderpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenderpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenderpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renderpractice/renderpractice/RenderpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renderpractice.renderpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
